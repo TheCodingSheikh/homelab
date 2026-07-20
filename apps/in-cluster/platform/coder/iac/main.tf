de2921b1f41bee23b76e74e14919c572de1e399a
TheCodingSheikh <thecodingsheikh@github.com>
2026-07-21 00:31:09 +0300

    df

--- apps/in-cluster/platform/coder/iac/main.tf
+++ apps/in-cluster/platform/coder/iac/main.tf
@@ -2,6 +2,7 @@ resource "coderd_template" "kubernetes" {
   name         = "kubernetes"
   display_name = "Kubernetes"
   description  = "Provision Kubernetes Deployments as Coder workspaces."
+  icon         = "/icon/code.svg"
   versions = [{
     directory = "${path.module}/kubernetes"
     active    = true
@@ -12,6 +13,7 @@ resource "coderd_template" "kubernetes_envbox" {
   name         = "kubernetes-envbox"
   display_name = "Kubernetes (envbox)"
   description  = "Provision envbox pods as Coder workspaces."
+  icon         = "/icon/docker.svg"
   versions = [{
     directory = "${path.module}/kubernetes-envbox"
     active    = true
